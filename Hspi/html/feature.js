63a0d9fccebb10fc27ada8c062665701e04cf162
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2022-07-17 22:58:10 -0700

    Update Hspi/html/feature.js

--- Hspi/html/feature.js
+++ Hspi/html/feature.js
@@ -9,7 +9,7 @@ function getUrlParameter(sParam) {
         i;
 
     for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+        sParameterName = sURLVariables[i].split("=");
 
         if (sParameterName[0] === sParam) {
             return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
